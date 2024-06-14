55fa81d086275ebdd80c11fb747f85671ba286b5
Ryan Jin <ryanjin007@gmail.com>
2024-06-15 00:25:14 +0200

    Adding a second layer to the network

--- main.py
+++ main.py
@@ -11,16 +11,24 @@ inputs = [
     [-1.5, 2.7, 3.3, -0.8],
 ]
 
-# Weights for each neuron
-weights = [
+# Layer 1
+weights1 = [
     [0.2, 0.8, -0.5, 1],
     [0.5, -0.91, 0.26, -0.5],
     [-0.26, -0.27, 0.17, 0.87],
 ]
 
-# Bias for each neuron
-biases = [2, 3, 0.5]
+biases1 = [2, 3, 0.5]
 
-outputs = np.dot(inputs, np.array(weights).T) + biases
+# Layer 2
+weights2 = [
+    [0.1, -0.14, 0.5],
+    [-0.5, 0.12, -0.33],
+    [-0.44, 0.73, -0.13],
+]
+biases2 = [-1, 2, -0.5]
+
+layer1_output = np.dot(inputs, np.array(weights1).T) + biases1
+outputs = np.dot(layer1_output, np.array(weights2).T) + biases2
 
 print(outputs)
