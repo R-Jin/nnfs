f71f3d53d2bfc7abf29be5b4d765b50e61a2bd62
Ryan Jin <ryanjin007@gmail.com>
2024-06-14 20:07:58 +0200

    Neural network with one layer
    The layer consists of three neurons

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
-inputs = [1, 2, 3, 2.5]
-weights1 = [0.2, 0.8, -0.5, 1]
-weights2 = [0.5, -0.91, 0.26, -0.5]
-weights3 = [-0.26, -0.27, 0.17, 0.87]
+"""Math library"""
 
-bias1 = 2
-bias2 = 3
-bias3 = 0.5
+import numpy as np
 
-weights = [weights1, weights2, weights3]
-biases = [bias1, bias2, bias3]
+# Neural network one layer of three neurons
 
-outputs = []
+# One batch with three samples
+inputs = [
+    [1, 2, 3, 2.5],
+    [2, 5, -1, 2],
+    [-1.5, 2.7, 3.3, -0.8],
+]
 
-for neuron_weights, bias in zip(weights, biases):
-    output = 0
-    for n_input, weight in zip(inputs, neuron_weights):
-        output += n_input * weight
-    output += bias
-    outputs.append(output)
+# Weights for each neuron
+weights = [
+    [0.2, 0.8, -0.5, 1],
+    [0.5, -0.91, 0.26, -0.5],
+    [-0.26, -0.27, 0.17, 0.87],
+]
+
+# Bias for each neuron
+biases = [2, 3, 0.5]
+
+outputs = np.dot(inputs, np.array(weights).T) + biases
 
 print(outputs)
